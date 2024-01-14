34871e1748405441a5d92d41538c61d22eaa13fd
Junaid Syed <sjunaid626@gmail.com>
2024-01-14 20:26:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDYYjiVZcG4_JnM0z34fydZbcbm3UqwECY',
   authDomain: 'tweetx-webapp.firebaseapp.com',
@@ -14,6 +10,7 @@ const firebaseConfig = {
   appId: '1:979207486714:web:657c9cdb0ea065497f9d43',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default db = getFirestore(app);
+
+export const auth = getAuth(app);
+export default app;
