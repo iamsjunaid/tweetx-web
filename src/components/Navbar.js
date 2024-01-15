64f0452bdbed50a625c087a0cd7963740f03af0c
Junaid Syed <sjunaid626@gmail.com>
2024-01-15 18:36:11 +0530

    Create skeleton of navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <div>
+      <h1>Logo</h1>
+    </div>
+    <ul>
+      <li>Home</li>
+      <li>Profile</li>
+      <li>Logout</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
