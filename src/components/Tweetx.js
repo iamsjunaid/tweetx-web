c4ee6620a1e29432bdf81d22f46266521694c462
Junaid Syed <sjunaid626@gmail.com>
2024-01-15 18:36:23 +0530

    Create homepage skeleton

--- src/components/Tweetx.js
+++ src/components/Tweetx.js
@@ -2,18 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase';
+import Navbar from './Navbar';
 
 const Tweetx = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
         const { uid } = user;
-        // ...
-        console.log('uid', uid);
       } else {
         // User is signed out
         // ...
@@ -24,16 +22,15 @@ const Tweetx = () => {
 
   const handleLogout = () => {
     signOut(auth).then(() => {
-    // Sign-out successful.
       navigate('/');
-      console.log('Signed out successfully');
     }).catch((error) => {
       setError(error.message);
     });
   };
 
   return (
-    <div>
+    <div className="text-center bg-pink-400">
+      <Navbar />
       {error ? <p>{error}</p> : null}
       <h1> Tweetx </h1>
       <button type="button" onClick={handleLogout}>Logout</button>
