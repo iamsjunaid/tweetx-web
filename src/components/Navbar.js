ca629ee989e8f342aa2c260e5d75762cba695ed0
Junaid Syed <sjunaid626@gmail.com>
2024-01-16 11:45:14 +0530

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Navbar = () => (
-  <nav>
-    <div>
-      <h1>Logo</h1>
+  <nav className="flex justify-around items-center px-12 py-4 shadow-md">
+    <div className=" text-pink-400 text-3xl font-semibold">
+      <h1>TweetX</h1>
     </div>
-    <ul>
-      <li>Home</li>
-      <li>Profile</li>
-      <li>Logout</li>
+    <ul className="flex list-none justify-around gap-12">
+      <li className="font-bold text-xl text-gray-500">Feed</li>
+      <li className="font-bold text-xl text-gray-500">Users</li>
+      <li className="font-bold text-xl text-gray-500">Profile</li>
     </ul>
   </nav>
 );
